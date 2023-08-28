8e5dad6445c3ae3b67a98320defd7cf94ed5e42c
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-08-28 05:57:29 -0400

    Manipulate DOM

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Test 1
+const main = document.getElementById('main');
+main.remove();
+
+// Test 2
+const newHeader = document.createElement('h1');
+
+// Test 3
+newHeader.id = 'victory';
+
+//Test 4
+newHeader.textContent = `SEAN is the champion`
